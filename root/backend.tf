193b8016d31334ffed9ade788431e64eeec7f83f
sameerbehera10 <sameerbehera10@outlook.com>
2025-02-17 21:06:52 +0530

    Update backend.tf

--- root/backend.tf
+++ root/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate-piyush-101"
+    bucket = "test-this-terraform1"
     key    = "backend/10weeksofcloudops-demo.tfstate"
     region = "us-east-1"
     dynamodb_table = "remote-backend"
